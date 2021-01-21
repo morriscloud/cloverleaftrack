e7c555cba8dd76a3ed23829c3ba1360ad6b89677
Tony Morris <anthony.frederick.morris@gmail.com>
2021-01-20 20:43:45 -0500

    More add

--- infra/main.tf
+++ infra/main.tf
@@ -89,20 +89,20 @@ resource "cloudflare_zone_settings_override" "this" {
   }
 }
 
-//resource "cloudflare_record" "validation" {
-//  for_each = {
-//    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
-//      name   = dvo.resource_record_name
-//      record = dvo.resource_record_value
-//      type   = dvo.resource_record_type
-//    }
-//  }
-//
-//  name    = each.value.name
-//  value   = trimsuffix(each.value.record, ".")
-//  type    = each.value.type
-//  zone_id = cloudflare_zone.this.id
-//}
+resource "cloudflare_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  value   = trimsuffix(each.value.record, ".")
+  type    = each.value.type
+  zone_id = cloudflare_zone.this.id
+}
 
 //resource "cloudflare_record" "cname" {
 //  count = length(module.cloudfront.cloudfront_domain_names)
@@ -113,12 +113,12 @@ resource "cloudflare_zone_settings_override" "this" {
 //  zone_id = cloudflare_zone.this.id
 //}
 
-//resource "aws_acm_certificate_validation" "this" {
-//  provider = aws.us-east-1
-//
-//  certificate_arn         = aws_acm_certificate.this.arn
-//  validation_record_fqdns = [for record in cloudflare_record.validation : record.hostname]
-//}
+resource "aws_acm_certificate_validation" "this" {
+  provider = aws.us-east-1
+
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in cloudflare_record.validation : record.hostname]
+}
 
 resource "aws_acm_certificate" "this" {
   provider = aws.us-east-1
